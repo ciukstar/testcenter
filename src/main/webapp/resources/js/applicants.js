c9a3067e01b594b544310fe4901d87791fe6bfbd
Sergiu Starciuc <ciukstar@yahoo.com>
2016-09-30 20:37:18 +0300

    Add Applicant action toolbar

--- src/main/webapp/resources/js/applicants.js
+++ src/main/webapp/resources/js/applicants.js
@@ -1,21 +1,37 @@
-$('#applicants_list').puidatatable({
-	caption: 'Applicants',
-	paginator: { rows: 10 },
-	columns: [
-		{field: 'surname', headerText: 'Surname'},
-		{field: 'name', headerText: 'Name'},
-		{field: 'patronymic', headerText: 'Patronymic'}
-	],
-	datasource: function(callback) {
-		$.ajax({
-			type: "GET",
-			url: 'applicants',
-			dataType: "json",
-			context: this,
-			success: function(response) {
-				callback.call(this, response);
-			}
-		});
+$(function() {
+	
+	$('.create_button').puibutton({
+		icon: 'fa-plus'
+	});
+	
+	$('.edit_button').puibutton({
+		icon: 'fa-pencil'
+	});
+	
+	$('.delete_button').puibutton({
+		icon: 'fa-trash'
+	});
+
+	$('#applicants_list').puidatatable({
+		caption: 'Applicants',
+		paginator: { rows: 10 },
+		columns: [
+			{field: 'surname', headerText: 'Surname'},
+			{field: 'name', headerText: 'Name'},
+			{field: 'patronymic', headerText: 'Patronymic'}
+		],
+		datasource: function(callback) {
+			$.ajax({
+				type: "GET",
+				url: 'applicants',
+				dataType: "json",
+				context: this,
+				success: function(response) {
+					callback.call(this, response);
+				}
+			});
 	}
+});
+
 });
 
