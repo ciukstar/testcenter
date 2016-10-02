ef942a557433a5490eb9fa8f853f86dcc9db1d73
Sergiu Starciuc <ciukstar@yahoo.com>
2016-10-03 00:34:44 +0300

    'TODO' fix the error

--- src/main/scala/code/lib/ApplicantService.scala
+++ src/main/scala/code/lib/ApplicantService.scala
@@ -19,8 +19,10 @@ object ApplicantService extends RestHelper {
   serve("api" / "applicants" prefix {
     case id :: Nil JsonGet req =>
       Applicant.find(id).map(_.toJson)
-    case Nil JsonPost req => 
-      Applicant.fromJson(req._1.asInstanceOf[JObject]).map{ a => a.save; a.toJson }
+    case Nil JsonPost req => { 
+      val a: Option[Applicant] = Applicant.fromJson(req._1.asInstanceOf[JObject]).map{ a => a.save; a }
+      JsonResponse(a.get.toJson, List(("Location", "api/applicants/" + a.get.id.value)), Nil, 201)
+    }
     case Nil JsonGet _ =>
       JsonResponse(Applicant.findAllAsJson)
     case _ => <p>not found</p>
